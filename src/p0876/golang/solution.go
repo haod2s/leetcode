7083ad886515dab9168b5aee79cea73e4ff9f11b
Holden <holden_s@163.com>
2020-12-22 21:47:26 +0800

    Commit no.141 no.142 no.876

---
+++ src/p0876/golang/solution.go
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast, slow = fast.Next.Next, slow.Next
+	}
+	return slow
+}
